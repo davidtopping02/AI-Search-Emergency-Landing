63b70dbdb74ff43bf71115fbaf1ce298cbaaf8fe
CalebH <caleb.harmon2345@gmail.com>
2022-03-14 23:19:59 +0000

    added api doc

--- mapsAPI.py
+++ mapsAPI.py
@@ -1,6 +1,7 @@
 import airportsdata
 from amadeus import Client, ResponseError
 
+#doc for the API https://developers.amadeus.com/self-service/category/air/api-doc/airport-nearest-relevant/api-reference
 #dont steal my API keys
 amadeus = Client(
     client_id='mOY4RkBAqfZVg6QNyYIh23NUkENb3IAF',
